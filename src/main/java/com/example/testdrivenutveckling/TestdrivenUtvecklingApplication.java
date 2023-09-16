b82398c6f22ac24abf6564bc74b229efd44fdd2f
Liver Nissan <storegame352@gmail.com>
2023-09-16 15:09:35 +0200

    försöker göra om koden

---
+++ src/main/java/com/example/testdrivenutveckling/TestdrivenUtvecklingApplication.java
@@ -0,0 +1,13 @@
+package com.example.testdrivenutveckling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestdrivenUtvecklingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestdrivenUtvecklingApplication.class, args);
+    }
+
+}
